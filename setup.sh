06857fe8c808230a4da5d7d44b4894f508520afc
Mayank Madan <mayank.madan@adpushup.com>
2021-09-18 14:58:59 +0530

    Fixes to scripts

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
-
 # Create ApAdmin user
-sudo useradd apadmin
+sudo useradd -m -s /bin/bash apadmin
+
+# Create workdir for this script
+mkdir -p /etc/adpushup
 
 # Download setup scripts
-gsutil -m cp -r gs://logger-jar-bucket/remotedevenv /
+gsutil -m cp -r gs://logger-jar-bucket/remotedevenv /etc/adpushup
 
-cd /remotedevenv
+cd /etc/adpushup/remotedevenv
 
 sudo chmod 700 setup
 
-sh setup
+/bin/bash setup
