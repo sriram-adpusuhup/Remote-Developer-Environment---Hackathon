a453d7df37cd2fbf07f078693790aa01140bcb67
Mayank Madan <mayank.madan@adpushup.com>
2021-09-18 15:18:10 +0530

    Add Password for apadmin

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 # Create ApAdmin user
 sudo useradd -m -s /bin/bash apadmin
 
+PASSWORD=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/instance-password -H "Metadata-Flavor: Google")
+
+# Set password for apadmin
+echo -e "$PASSWORD\n$PASSWORD" | passwd apadmin
+
 # Create workdir for this script
 mkdir -p /etc/adpushup
 
